3766a638de371b3165edbf0f4390609c44ff6866
ge69zaq <ge69zaq@gitlab.lrz.de>
2019-12-03 17:27:31 +0100

    slight improvement

--- antenna.py
+++ antenna.py
@@ -4,7 +4,6 @@ import sys
 import psycopg2
 import requests
 from bs4 import BeautifulSoup
-from bs4 import Comment
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 '
