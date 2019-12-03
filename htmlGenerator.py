8c0ef4ec89f06161a257753511f4bf92c8136fae
ge69zaq <ge69zaq@gitlab.lrz.de>
2019-12-03 16:18:47 +0100

    simplify the code

--- htmlGenerator.py
+++ htmlGenerator.py
@@ -1,8 +1,10 @@
 from yattag import Doc
 import report
 
-newInfo = report.recordList
+newInfo = report.newRecordList
 history = report.historyList_pair
+aDict = report.antennaCounter_dict
+vDict = report.vsnCounter_dict
 
 
 def create_file(file_name):
@@ -20,7 +22,7 @@ doc, tag, text, line = Doc().ttl()
 # tag = doc.tag
 # text = doc.text
 
-#########################################################################
+#################################latestAntenna.html########################################
 with tag('html'):
     with tag('body'):
         with tag ('table', width="100%", border="1", bgcolor="#FFFFFF", cellpadding="0", cellspacing="0", align="left"):
@@ -28,18 +30,27 @@ with tag('html'):
                 with tag('th', scope="col", bgcolor="#D6E3BC", colspan="8", style="font-size:14pt;"):
                     text('The latest information of each antenna')
             with tag('tr'):     # row of names
-                title_name = ( 'Station', 'Schedule', 'A or B', 'VSN', 'TIME', 'REMAINING GB', 'REMAINING PERCENT(%)')
+                title_name = ( 'Station', 'Schedule', 'HDD SLOT', 'VSN', 'TIME', 'REMAINING GB', 'REMAINING PERCENT(%)')
                 for tn in title_name:
                     with tag('td', bgcolor="#FFFFFF",
                              style="font-size:11pt;""font-weight: bold;", align="center", width="160"):
                         # setting bold differently in style
                         with tag('font', color="#000000"):
                             text(tn)
+            # avoid duplication
+            seen = []
             for row in newInfo:
                 with tag('tr'):     # every row except the title_row
-                    for element in row:
+                    rowspan = str(aDict[row[0]])
+                    if row[0] not in seen:
+                        seen.append(row[0])
+                        with tag('td',bgcolor="#FFFFFF", rowspan=rowspan,
+                                 style="font-size:11pt;}", align="center", width="160"):
+                            text(str(row[0]))
+
+                    # offset == 1
+                    for element in row[1:]:
                         with tag('td', bgcolor="#FFFFFF", style="font-size:11pt;", align="center", width="160"):
-                            # setting bold differently in style
                             with tag('font', color="#000000"):
                                 text(str(element))      # convert every element in the mixed tuple to str
 
@@ -48,25 +59,33 @@ result = doc.getvalue()
 create_file("latestAntenna.html")
 
 doc, tag, text, line = Doc().ttl()
-#######################################################################
+################################historyVSN.html#######################################
 with tag('html'):
     with tag('body'):
         with tag ('table', width="100%", border="1", bgcolor="#FFFFFF", cellpadding="0", cellspacing="0", align="left"):
             with tag('tr'):     # row of title
                 with tag('th', scope="col", bgcolor="#D6E3BC", colspan="9", style="font-size:14pt;"):
-                    text('The history of each VSN-Antenna Pair')
+                    text('The history of VSN Usage')
             with tag('tr'):     # row of names
-                title_name = ( 'VSN', 'STATION', 'Schedule', 'A or B', 'TIME', 'REMAINING GB', 'REMAINING PERCENT(%)',
-                               'GENERATED TIME', 'EXISTENCE')
+                title_name = ('VSN', 'STATION', 'Schedule', 'HDD SLOT', 'TIME', 'REMAINING GB', 'REMAINING PERCENT(%)',
+                              'GENERATED TIME', 'STATUS')
                 for tn in title_name:
                     with tag('td', bgcolor="#FFFFFF",
                              style="font-size:11pt;""font-weight: bold;", align="center", width="160"):
                         # setting bold differently in style
                         with tag('font', color="#000000"):
                             text(tn)
+            # avoid duplication of VSN name
+            seen = []
             for row in history:
                 with tag('tr'):     # every row except the title_row
-                    for element in row:
+                    rowspan = str(vDict[row[0]])
+                    if row[0] not in seen:
+                        seen.append(row[0])
+                        # each VSN name is only drawn once
+                        with tag('td',bgcolor="#FFFFFF", rowspan=rowspan, style="font-size:11pt;", align="center", width="160"):
+                            text(str(row[0]))
+                    for element in row[1:]:
                         with tag('td', bgcolor="#FFFFFF", style="font-size:11pt;", align="center", width="160"):
                             # setting bold differently in style
                             with tag('font', color="#000000"):
--- report.py
+++ report.py
@@ -3,76 +3,71 @@
 import connect as conn
 
 cursor = conn.cursor
-cursor.execute('''SELECT count(DISTINCT whichAntenna) 
-                  FROM capacity''')
-# get the integer because of the following iteration
-numAntenna = cursor.fetchall()[0][0]
-#print(numAntenna)
 
-recordList = []
+newRecordList = []
 historyList_pair = []
-historyList_VSN = []
+historyList_VSN_lastState = []
+# to count identical antenna names for output format in .html
+antennaCounter_dict = {}
+vsnCounter_dict = {}
 
-# offset starts with 0
-for i in range(numAntenna):
-    # each iteration is for one Antenna
-    cursor.execute('''SELECT DISTINCT whichAntenna
-                      FROM capacity
-                      ORDER BY whichAntenna
-                      LIMIT 1 OFFSET %s''', str(i))
 
-    antennaName = cursor.fetchall()
-    #print(antennaName)
 
-    # the latest record
-    cursor.execute('''SELECT whichAntenna, Schedule, AorB, VSN, mtime, remainingGB, remainingPer
-                      FROM capacity
-                      WHERE whichAntenna = %s AND AorB = 'A'
-                      ORDER BY created_at DESC
-                      LIMIT 1''', antennaName)
-    lastRecord = cursor.fetchall()
-    for lr in lastRecord:
-        recordList.append(lr)
+# DISTINCT ON is for PostSQL only
 
-    cursor.execute('''SELECT whichAntenna, Schedule, AorB, VSN, mtime, remainingGB, remainingPer
-                      FROM capacity
-                      WHERE whichAntenna = %s AND AorB = 'B'
-                      ORDER BY created_at DESC
-                      LIMIT 1''', antennaName)
-    lastRecord = cursor.fetchall()
-    # to break the square brackets, namely avoid nested list
-    for lr in lastRecord:
-        recordList.append(lr)
+cursor.execute('''SELECT DISTINCT ON (whichAntenna, AorB)
+                      whichAntenna, Schedule, AorB, VSN, mtime, remainingGB, remainingPer
+                  FROM capacity
+                  ORDER BY whichAntenna, AorB, created_at DESC ''')
+lastRecords = cursor.fetchall()
+for lr in lastRecords:
+    newRecordList.append(lr)
+    print(lr)
 
-    # DISTINCT ON is for PostSQL only
-    # all unique combinations of VSN-Antenna-Schedule
-    cursor.execute('''SELECT DISTINCT ON (VSN, whichAntenna, schedule)
-                          VSN, whichAntenna, Schedule, AorB, mtime, remainingGB, remainingPer, created_at
-                      FROM capacity
-                      WHERE whichAntenna = %s
-                      ORDER BY VSN, whichAntenna, schedule, created_at DESC 
-                      ''', antennaName)
-    VSN_Antenna_Schedule_history = cursor.fetchall()
-    for vah in VSN_Antenna_Schedule_history:
-        historyList_pair.append(vah)
+cursor.execute('''SELECT DISTINCT whichAntenna FROM capacity 
+                  ORDER BY whichAntenna ASC''')
+antenna_raw = cursor.fetchall()
+# initialize antennaCounter_dict
+for a in antenna_raw:
+    antennaCounter_dict[a[0]] = 0
+for aTuple in newRecordList:
+    antennaCounter_dict[aTuple[0]] += 1
 
-    # if some VSN show more than once, (but VSN-Antenna-Schedule pairs are unique),
-    # it means these VSN are used more than once
-    cursor.execute('''SELECT DISTINCT ON (VSN)
-                          VSN, whichAntenna, Schedule, AorB, mtime, remainingGB, remainingPer, created_at
-                      FROM capacity
-                      WHERE whichAntenna = %s
-                      ORDER BY VSN, created_at DESC 
-                      ''', antennaName)
-    VSN_history = cursor.fetchall()
-    for vh in VSN_history:
-        historyList_VSN.append(vh)
+# if some VSN show more than once, (but VSN-Antenna-Schedule pairs are unique),
+# it means these VSN are used more than once
+# all unique VSN means also all newest VSN
+cursor.execute('''SELECT DISTINCT ON (VSN)
+                      VSN, whichAntenna, Schedule, AorB, mtime, remainingGB, remainingPer, created_at
+                  FROM capacity
+                  ORDER BY VSN, created_at DESC ''')
+VSN_lastState = cursor.fetchall()
+for vl in VSN_lastState:
+    historyList_VSN_lastState.append(vl)
 
+# all unique combinations of VSN-Antenna-Schedule
+# because even the same VSN-Antenna combination could have not only one schedule
+cursor.execute('''SELECT DISTINCT ON (VSN, whichAntenna, schedule)
+                      VSN, whichAntenna, Schedule, AorB, mtime, remainingGB, remainingPer, created_at
+                  FROM capacity
+                  ORDER BY VSN, whichAntenna, schedule, created_at DESC 
+                  ''')
+VSN_Antenna_Schedule_history = cursor.fetchall()
+for vah in VSN_Antenna_Schedule_history:
+    historyList_pair.append(vah)
+
+cursor.execute('''SELECT DISTINCT VSN FROM capacity''')
+VSN_raw = cursor.fetchall()
+
+# initialize vsnCounter_dict
+for vsn in VSN_raw:
+    vsnCounter_dict[vsn[0]] = 0
+for aTuple in historyList_pair:
+    vsnCounter_dict[aTuple[0]] += 1
 for i in range(len(historyList_pair)):
-    if historyList_pair[i] not in historyList_VSN:
+    if historyList_pair[i] not in historyList_VSN_lastState:
         historyList_pair[i] += ("Out of date",)
     else:
-        historyList_pair[i] += ("Overwritten",)
+        historyList_pair[i] += ("latest",)
 
 # Alternative
     # cursor.execute('''SELECT m.VSN, m.mtime, m.remainingGB, m.remainingPer, t.create_t
@@ -85,3 +80,48 @@ for i in range(len(historyList_pair)):
     #                   ''', antennaName)
     # VSN_history = cursor.fetchall()
     # print(VSN_history)
+    #
+    # cursor.execute('''SELECT count(DISTINCT whichAntenna)
+    #                   FROM capacity''')
+    # # get the integer because of the following iteration
+    # numAntenna = cursor.fetchall()[0][0]
+    # print(numAntenna)
+
+    # offset starts with 0
+    # for i in range(numAntenna):
+    #     # each iteration is for one Antenna
+    #     cursor.execute('''SELECT DISTINCT whichAntenna
+    #                       FROM capacity
+    #                       ORDER BY whichAntenna
+    #                       LIMIT 1 OFFSET %s''', str(i))
+    #
+    #     antennaName = cursor.fetchall()
+    #     # print(antennaName)
+    #
+    #     # the latest record
+    #     cursor.execute('''SELECT whichAntenna, Schedule, AorB, VSN, mtime, remainingGB, remainingPer
+    #                       FROM capacity
+    #                       WHERE whichAntenna = %s AND AorB = 'A'
+    #                       ORDER BY created_at DESC
+    #                       LIMIT 1''', antennaName)
+    #     lastRecord = cursor.fetchall()
+    #     # Although length exists throughout, it is always overwritten each time before usage
+    #     length = len(lastRecord)
+    #     antennaCounter_dict[antennaName[0][0]] = length
+    #     for lr in lastRecord:
+    #         newRecordList.append(lr)
+    #         print(lr)
+    #
+    #     cursor.execute('''SELECT whichAntenna, Schedule, AorB, VSN, mtime, remainingGB, remainingPer
+    #                       FROM capacity
+    #                       WHERE whichAntenna = %s AND AorB = 'B'
+    #                       ORDER BY created_at DESC
+    #                       LIMIT 1''', antennaName)
+    #     lastRecord = cursor.fetchall()
+    #     # to break the square brackets, namely avoid nested list
+    #     length = len(lastRecord)
+    #     antennaCounter_dict[antennaName[0][0]] += length
+    #     for lr in lastRecord:
+    #         newRecordList.append(lr)
+    #         print(lr)
+
