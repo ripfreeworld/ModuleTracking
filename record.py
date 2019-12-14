2641663247812292093c88072e5612b50b58bfd5
ge69zaq <ge69zaq@gitlab.lrz.de>
2019-12-14 16:16:27 +0100

    modify the schedule reading

--- record.py
+++ record.py
@@ -21,7 +21,7 @@ cursor.execute('''create table IF NOT EXISTS capacity (
                         remainingPer decimal(3,1) NOT NULL,
                         checkTime time(0) NOT NULL,
                         created_at TIMESTAMPTZ DEFAULT Now(),
-                        schedule varchar(20),
+                        schedule varchar(20) NOT NULL,
                         CONSTRAINT no_duplicate UNIQUE (VSN, mtime, checkTime, remainingGB)
                                                   )''')
                         # the constraint name `no_duplicate` can exist only once in the entire database
@@ -41,16 +41,18 @@ while True:
     # Each station has its own url, which only differs in one segment, i.e. /.../
     for stationName in stationNames:
         # tuple
-        stationName = stationName[0]
+        stationName = stationName[0].lower()
         # .lower() is to convert uppercase to lowercase e.g. WETTZELL -> wettzell
-        url = f"https://vlbisysmon.evlbi.wettzell.de/monitoring_archive/fs_web_pages/{stationName.lower()}" \
+        url = f"https://vlbisysmon.evlbi.wettzell.de/monitoring_archive/fs_web_pages/{stationName}" \
             f"/Mark5RemainingCapacity.html"
-        url2readSchedule = f"https://vlbisysmon.evlbi.wettzell.de/monitoring_archive/fs_web_pages/ivsquickstatus.html"
+        url2readSchedule = f"https://vlbisysmon.evlbi.wettzell.de/monitoring_archive/fs_web_pages/{stationName}" \
+            f"/eQuickStatusReport.txt"
 
         page = requests.get(url, headers=headers)
         page2readSchedule = requests.get(url2readSchedule, headers=headers)
 
         soup = BeautifulSoup(page.text, 'html.parser')
+        # in order to find the corresponding schedule and add to tuple
         soup2readSchedule = BeautifulSoup(page2readSchedule.text, 'html.parser')
 
         moduleAB = None
@@ -95,22 +97,25 @@ while True:
         if moduleAB is None:
             print("Neither Module A nor Module B is selected")
 
-        table = soup2readSchedule.find(lambda tag: tag.name == 'table')
-        # this is the way to locate antenna code lack of labels
-        rows = table.findAll(lambda tag: tag.name == 'tr')
+        content = soup2readSchedule.text
+        list_content = content.split()
         schedule = None
-        for row in rows:
-            antenna_links = row.findAll('a')
-            for antenna_link in antenna_links:
-                parent = antenna_link.parent
-                antennaName = antenna_link.text.strip()
-                vsn = parent.find_next_siblings()[7].text.strip()
-                if antennaName == stationName and vsn == VSN:
-                    schedule = parent.find_next_siblings()[4].text.strip()
-                if schedule == '':
-                    schedule = None
+
+        try:
+            vsn = list_content[list_content.index("Mark5VSN") + 2]
+            if vsn == VSN : # to make sure that the VSN has not changed at the reading moment
+                schedule_index = list_content.index("Schedule ")
+                # avoid the situation that schedule is empty. It wouldn't have an empty sting " ", but just nothing
+                if list_content[schedule_index + 3] is not "=":
+                    schedule = list_content[schedule_index + 2]
+                    # if schedule is empty, it would keep being None
+        except:
+            print(f"cannot get information from {stationName} at the moment")
+
         data = [(stationName, moduleAB, VSN, mtime, remainingGB, remainingPer, checkTime, schedule)]
         print("current information", data)
+        # don't save the information with empty schedule, otherwise it generates redundant data with empty schedule
+        # as a new schedule label
         # insert into the table capacity
         try:  # ON CONFLICT (stationName, moduleAB, VSN, mtime, checkTime, remainingGB) DO NOTHING
             cursor.execute("insert into capacity(id, whichAntenna, hdd_slot, VSN, mtime, remainingGB, remainingPer, "
